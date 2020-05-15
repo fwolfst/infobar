05ca3af116a023767d38627d7088aae768ca14ce
Florian Frank <flori@ping.de>
2020-05-15 17:43:50 +0200

    use keyword operator

--- lib/infobar.rb
+++ lib/infobar.rb
@@ -87,7 +87,7 @@ class Infobar
   def busy(**opts)
     block_given? or raise ArgumentError, 'block is required as an argument'
     duration = opts.delete(:sleep) || 0.1
-    call(opts | {
+    call(**opts | {
       total: Float::INFINITY,
       message: { format: ' %l %te %s ', '%s' => { frames: :circle1 } },
     })
